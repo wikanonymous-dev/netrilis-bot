7535c521bda40452c7ea65f5a66013f91e22a528
wikanonymous-dev <alesaeza@gmail.com>
2026-07-16 11:08:48 +0700

    fix: prever sendMessage after job artifact successfully deleted

--- src/index.ts
+++ src/index.ts
@@ -12,92 +12,81 @@ app.use(express.json())
 
 // GitLab webhook — sends notifications to the CICD topic
 app.post('/webhook/gitlab', async (req: Request, res: Response) => {
-  const event = req.header('X-Gitlab-Event')
-  const body = req.body
-
-  let messageText = ''
-  let topicWebhook = null
-  let topicMessage = ''
-
-  switch (event) {
-    case 'Merge Request Hook':
-      const mergeAction = body.object_attributes.action
-
-      if (mergeAction === 'merge') {
-        messageText = getMergeRequestTemplateText({
-          url: String(body.object_attributes.url),
-          source_branch: String(body.object_attributes.source_branch),
-          target_branch: String(body.object_attributes.target_branch),
-          author: String(body.user.username),
-          description: String(body.object_attributes.description)
-        })
-      }
-      break;
-
-    case 'Pipeline Hook':
-      const isFailed = body.object_attributes.status === 'failed'
-      const isSuccess = body.object_attributes.status === 'success'
-      const isBuildSuccess = body.builds.at(0).status === 'success'
-      const isDeploySuccess = body.builds.at(-1).status === 'success'
-
-      const threadVariable = body.variables.find((v: { key: string; value: string }) => v.key === 'TELEGRAM_THREAD_ID')
-
-      if (threadVariable) topicWebhook = 'ops'
-
-      if (isFailed) {
-        messageText = getJobTemplateText( '🚀 Deploy Failed', {
-          ref: String(body.object_attributes.ref),
-          url: String(body.object_attributes.url),
-          triggerer: String(body.user.username)
-        })
-      } else if (isSuccess && isBuildSuccess && isDeploySuccess) {
-        messageText = getJobTemplateText( '🚀 Deploy Success', {
-          ref: String(body.object_attributes.ref),
-          url: String(body.object_attributes.url),
-          triggerer: String(body.user.username)
-        })
-      }
-      break;
-
-    case 'Job Hook':
-      if (body.object_kind === 'build') {
-        const jobId = body.build_id
-        const tag = body.ref
-        const jobName = body.build_name
-        const status = body.build_status
-
-        const artifactPath = `artifact/job-${jobId}-attributes.json`
-        removeArtifact(artifactPath)
-          .then(async () => {
-            topicWebhook = 'ops'
-            topicMessage = `🚀 *Job re-triggered*\n\nTag: \`${tag}\`\nJob: \`${jobName}\`\nStatus: \`${status}\``
-
-            console.info('✅ Job artifact file removed at:', artifactPath);
-          })
-          .catch((error) => {
-            if (error.code === 'ENOENT') {
-              console.info('ℹ️ Job artifact file does not exist. No cleanup needed.');
-            } else {
-              console.error('❌ An unexpected error occurred:', error.message);
-            }
-
-            topicWebhook = null
-          })
-      }
-
-      break;
-
-    default:
-      break;
-  }
-
   try {
+    const event = req.header('X-Gitlab-Event')
+    const body = req.body
+
+    let messageText = ''
+
+    switch (event) {
+      case 'Merge Request Hook':
+        const mergeAction = body.object_attributes.action
+
+        if (mergeAction === 'merge') {
+          messageText = getMergeRequestTemplateText({
+            url: String(body.object_attributes.url),
+            source_branch: String(body.object_attributes.source_branch),
+            target_branch: String(body.object_attributes.target_branch),
+            author: String(body.user.username),
+            description: String(body.object_attributes.description)
+          })
+        }
+        break;
+
+      case 'Pipeline Hook':
+        const isFailed = body.object_attributes.status === 'failed'
+        const isSuccess = body.object_attributes.status === 'success'
+        const isBuildSuccess = body.builds.at(0).status === 'success'
+        const isDeploySuccess = body.builds.at(-1).status === 'success'
+
+        if (isFailed) {
+          messageText = getJobTemplateText( '🚀 Deploy Failed', {
+            ref: String(body.object_attributes.ref),
+            url: String(body.object_attributes.url),
+            triggerer: String(body.user.username)
+          })
+        } else if (isSuccess && isBuildSuccess && isDeploySuccess) {
+          messageText = getJobTemplateText( '🚀 Deploy Success', {
+            ref: String(body.object_attributes.ref),
+            url: String(body.object_attributes.url),
+            triggerer: String(body.user.username)
+          })
+        }
+        break;
+
+      case 'Job Hook':
+        if (body.object_kind === 'build') {
+          const jobId = body.build_id
+          const tag = body.ref
+          const jobName = body.build_name
+          const status = body.build_status
+
+          const artifactPath = `artifact/job-${jobId}-attributes.json`
+          removeArtifact(artifactPath)
+            .then(async () => {
+              const topicMessage = `🚀 *Job re-triggered*\n\nTag: \`${tag}\`\nJob: \`${jobName}\`\nStatus: \`${status}\``
+
+              console.info('✅ Job artifact file removed at:', artifactPath);
+
+              await sendMessage(topicMessage, 'ops')
+            })
+            .catch((error) => {
+              if (error.code === 'ENOENT') {
+                console.info('ℹ️ Job artifact file does not exist. No cleanup needed.');
+              } else {
+                console.error('❌ An unexpected error occurred:', error.message);
+              }
+            })
+        }
+
+        break;
+
+      default:
+        break;
+    }
+
     if (messageText) {
       await sendMessage(messageText, 'cicd')
-
-      if (topicWebhook) {
-        await sendMessage(topicMessage, 'ops')
-      }
     }
   } catch (error) {
     console.error(error)
